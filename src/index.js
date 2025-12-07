b75da0baa4dd9b430ca41c6c42cf357d61e7033d
OrionRV6 <benjaminrodriguezvalladares@gmail.com>
2025-12-07 18:18:08 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// src/index.js
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Rutas
+const authRoutes = require('./routes/auth.routes');
+const productosRoutes = require('./routes/productos.routes');
+const redesRoutes = require('./routes/redes.routes');
+const paginasRoutes = require('./routes/paginas.routes');
+const idiomasRoutes = require('./routes/idiomas.routes');
+const usuariosRoutes = require('./routes/usuarios.routes');
+const carritoRoutes = require('./routes/carrito.routes');
+const ordenesRoutes = require('./routes/ordenes.routes'); 
+const notificacionesRoutes = require('./routes/notificaciones.routes');
+const resenasRoutes = require('./routes/resenas.routes');
+
+app.use('/auth', authRoutes);
+app.use('/productos', productosRoutes);
+app.use('/redes-sociales', redesRoutes);
+app.use('/paginas', paginasRoutes);
+app.use('/idiomas', idiomasRoutes);
+app.use('/usuarios', usuariosRoutes);
+app.use('/clientes', usuariosRoutes);
+app.use('/carrito', carritoRoutes);
+app.use('/ordenes', ordenesRoutes);  
+app.use('/notificaciones', notificacionesRoutes);
+app.use('/resenas', resenasRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Backend funcionando');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
